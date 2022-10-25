900ab39371cd3341f18af69c649b1e4f4704ec69
hkeena98 <hkeena98@gmail.com>
2022-10-25 07:18:28 -0400

    Project Identifier Model Implementation, and CSV Record Integration

--- collector.py
+++ collector.py
@@ -1,42 +1,85 @@
+"""
+"""
+
+# Declares Imports
 import sys
 import os
 import subprocess
 import time
+import csv
 
-def collector():
+from identifier import SourceIndentifier
+
+
+"""
+"""
+def project_identifier_collector():
     print("Collection...")
     root = "projects/"
     path = os.path.join(root, "")
     for path, subdirs, files in os.walk(root):
         for name in files:
-            if ".java" in name:
-                file_path = os.path.join(path, name)
-                project = file_path.split("/")[1]
+            file_path = os.path.join(path, name)
+            project = file_path.split("/")[1]
+            if name.lower().endswith(('.c', '.cpp', '.cc', '.java', '.cs', '.h')):
                 print("\nProject Name:", project, "\n")
                 print("File Name:", name, "\n")
                 print("File Path:", file_path, "\n")
+                if os.path.exists("reports/"+project) == False:
+                    subprocess.run(["mkdir", "reports/"+project])
+                    subprocess.run(["mkdir", "reports/"+project+"/srcml/"])
+                    subprocess.run(["mkdir", "reports/"+project+"/grabidentifiers/"])
                 if os.path.exists("reports/"+project):
                     print("Generating srcML .xml File...\n")
-                    subprocess.run(["srcml", file_path, "-o", "reports/"+project+"/"+name+".xml"])
+                    subprocess.run(["srcml", file_path, "-o", "reports/"+project+"/srcml/"+name+".xml"])
                     print("Grabbing Identifiers from srcML .xml File...\n")
-                    subprocess.run(["grabidentifiers", "-s", "10000", "reports/"+project+"/"+name+".xml", ">", "reports/"+project+"/"+name+".xml.log.txt"])
-                else:
-                    subprocess.run(["mkdir", "reports/"+project])
-                    print("Generating srcML .xml File...\n")
-                    subprocess.run(["srcml", file_path, "-o", "reports/"+project+"/"+name+".xml"])
-                    print("Grabbing Identifiers from srcML .xml File...\n")
-                    subprocess.run(["grabidentifiers", "-s", "10000", "reports/"+project+"/"+name+".xml", ">", "reports/"+project+"/"+name+".xml.log.txt"])
-                    #, ">", "reports/"+project+"/"+name+".xml.log.txt"
-                time.sleep(.5)
+                    gi_log_file = open("reports/"+project+"/grabidentifiers/"+name+".xml.log.txt", "w")
+                    subprocess.run(["grabidentifiers", "-s", "10000", "reports/"+project+"/srcml/"+name+".xml"], stdout=gi_log_file)
+                    gi_log_file.close()
+                    gi_log_file = open("reports/"+project+"/grabidentifiers/"+name+".xml.log.txt", "r")
+                    for i in gi_log_file.readlines()[:-1]:
+                        identifier = SourceIndentifier(i)
+                        identifier.print_identifier()
+                        append_project_csv(project, identifier)
+
+                    gi_log_file.close()
+                    
+                time.sleep(.05)
+
+
+"""
+"""
+def append_project_csv(project, identifier):
+    try: 
+        csv_file_name = "reports/"+project+"/"+project+"-identifiers.csv"
+        if os.path.exists(csv_file_name) == False:
+            header = ['VARIABLE_TYPE', 'IDENTIFIER_NAME', 'IDENTIFIER_USE_CASE', 'LANGUAGE', 'NUM', 'FILE_LOCATION']
+            with open(csv_file_name, 'w', encoding='UTF8', newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(header)
+        else:
+            id_tuple = (identifier.id_varType, identifier.id_name, identifier.id_useCase, identifier.id_lang, identifier.id_num, identifier.id_loc)
+            with open(csv_file_name, 'a', encoding='UTF8', newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(id_tuple)
+    except:
+        print("CSV FILE ERROR")
+
+"""
+"""
+def generate_dataset():
+    pass
+
+
+
                 
-    
-
-
-
+"""
+"""    
 def main():
     print("Beginning Preamble Collection...\n")
-    collector()
-    
 
+    project_identifier_collector()
+
+# Calls Main
 if __name__=='__main__':
     main()
---
+++ identifier.py
@@ -0,0 +1,37 @@
+"""
+"""
+class SourceIndentifier:
+    def __init__(self, file_line):
+        try:
+            parsed = self.parse_grabid_file(file_line)
+            self.id_varType = parsed[0] 
+            if len(parsed) == 5:
+                self.id_name = "UNDEFINED"
+                self.id_useCase = parsed[1]
+                self.id_lang = parsed[2]
+                self.id_num = parsed[3]
+                self.id_loc = parsed[4]               
+            else:
+                self.id_name = parsed[1]
+                self.id_useCase = parsed[2]
+                self.id_lang = parsed[3]
+                self.id_num = parsed[4]
+                self.id_loc = parsed[5]
+            
+
+        except:
+            print("IDENTIFIER PARSE ERROR")
+        
+    def parse_grabid_file(self, file_line):
+        parsed_lines = file_line.split()
+        print("Parsed Line:", parsed_lines)
+        return parsed_lines
+    
+    def print_identifier(self):
+        print("\nIDENTIFIER DETAILS\n")
+        print("Identifier Variable Type:", self.id_varType)
+        print("Identifier Name:", self.id_name)
+        print("Identifier Use Case:", self.id_useCase)
+        print("Identifier Language", self.id_lang)
+        print("Identifier Number:", self.id_num)
+        print("Identifier File Location", self.id_loc)
